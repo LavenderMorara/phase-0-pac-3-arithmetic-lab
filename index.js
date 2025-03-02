18a56a6a0b595060dafd67597553a303d9ae77ca
LavenderMorara <chiyamorara@gmail.com>
2025-03-02 14:56:21 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+let a;
+let b;
+function add(){
+    return a+b
+}
+console.log(add());
+function subtract(){
+    return a-b
+}
+console.log(subtract());
+function multiply(){
+    return a*b
+}
+console.log(multiply());
+function divide(){
+    return a/b
+}
+console.log(divide());
+function add(a,b){
+    return a+b
+}
+console.log(add());
+function subtract(a,b){
+    return a-b
+}
+console.log(subtract());
+function multiply(a,b){
+    return b*a
+}
+console.log(multiply());
+function divide(a,b){
+    return a/b
+}
+console.log(divide());
+
+function increment(n){
+    return n+=1
+}
+console.log(increment());
+function decrement(n){
+    return n-=1
+}
+console.log(decrement());
+
+function makeInt(n){
+    return parseInt(n)
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+  
